ff0ec7b9590bd25e8d83f9fa135ef64da26b8a4d
Andrew Glines and Hunter Parks <andrew@>
2017-04-26 16:00:25 -0700

    It will include all types of form input and change the background color based off of your selected color.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,8 @@
 // Back end logic
 
-
-// var music = function(input1 + input2){
-//   return ;
-// }
+var colorchange = function(color) {
+  document.body.style.backgroundColor = color;
+};
 
 
 
@@ -16,18 +15,26 @@ $(document).ready(function(){
   $("form#compute").submit(function(event){
     event.preventDefault();
 
+    // call a fn to reset form now
+    $("ul").empty();
+
     var whichButton = $("input:radio[name=which]:checked").val();
     var genre = $("#genre").val();
     var name = $("#namehere").val();
     var date = $("#born").val();
     var color = $("#color").val();
 
+    $("jumbotron").css('background-color', 'green');
+
     $("ul").append("<li>" + name + "</li>");
     $("ul").append("<li>" + whichButton + "</li>");
     $("ul").append("<li>" + genre + "</li>");
     $("ul").append("<li>" + date + "</li>");
     $("ul").append("<li>" + color + "</li>");
+    colorchange(color);
 
+
+    // $("ul" style="color: green").append("<li>test text color</li>");
 
   });
 });
